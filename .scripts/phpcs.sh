c71ede2a6f2f20460e69e8715cb39dc92aff6bf9
Danton Dietze Cancella <dantondietze@gmail.com>
2020-01-03 15:20:13 -0300

    Remmove WP standard param from phpcs script

--- .scripts/phpcs.sh
+++ .scripts/phpcs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-php $(pwd)/vendor/squizlabs/php_codesniffer/bin/phpcs --ignore=*/tests/*,/vendor/* --standard=WordPress --extensions=php -v -n --parallel=5 .
+php $(pwd)/vendor/squizlabs/php_codesniffer/bin/phpcs --ignore=*/tests/*,/vendor/* --extensions=php -v -n --parallel=5 .
 
  # we need this script to fail if any command fails
  # when a command fails the variable `err` will turn 1
