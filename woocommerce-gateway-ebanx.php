623ea8062659f7d0a8840f6b32e92323a26abc08
Diego Miranda Neves Gomes <diego.miranda.ng@gmail.com>
2021-01-25 14:45:55 -0300

    [EBX-2432] Add release changelog

--- woocommerce-gateway-ebanx.php
+++ woocommerce-gateway-ebanx.php
@@ -5,7 +5,7 @@
  * Description: Offer local payment methods
  * Author: EBANX Pay
  * Author URI: https://www.ebanxpay.com
- * Version: 3.4.2
+ * Version: 3.4.3
  * License: MIT
  * Text Domain: woocommerce-gateway-ebanx
  * Domain Path: /languages
